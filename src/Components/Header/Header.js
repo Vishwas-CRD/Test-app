ddc30a243a3fb32cec61ef3b8c0e361411bcc572
Vishwas-CRD <sachin.gowda@softsuave.com>
2021-02-09 18:03:14 +0530

    Initial Test Cheack

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div class='header_wrapper'>
+            <div class='wrap'>
+                <div class='logo_wrap'>
+                    <div class='logo_img'>
+                        Vishwas_@
+                    </div>
+
+                </div>
+
+            </div>
+ 
+
+            
+        </div>
+    )
+}
+
+export default Header
